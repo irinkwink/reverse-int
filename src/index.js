5fc9cd87b8d5dfc326b7262dbd16cced4458ee0b
irinkwink <irina-ns@yandex.ru>
2022-06-20 21:44:07 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let result = '' + Math.abs(n);    //toString
+    result = result.split('').reverse().join('');  //to array, reverse array and join into string
+    result = +result //toNumber (trim '0' in the begin)
+    return result;
 }
